af543a38c279fbb4a4d809625d64fb669eccd1b5
Cecilia Lau <cecilialau6776@gmail.com>
2026-02-24 16:51:16 -0500

    misc changes

--- app/controllers/api/cluster_status_controller.rb
+++ app/controllers/api/cluster_status_controller.rb
@@ -1,30 +1,29 @@
 module Api
   class ClusterStatusController < ApplicationController
     def get
-      nodes = Rails.cache.fetch("cluster_status", expires_in: 30.seconds, race_condition_ttl: 3.seconds) do
-        output, status = Open3.capture2("scontrol show node -a --oneliner")
-        
-        unless status.success?
-          raise "Failed to fetch cluster status"
-        end
-        
+      nodes = Rails.cache.fetch('cluster_status', expires_in: 30.seconds, race_condition_ttl: 3.seconds) do
+        output, status = Open3.capture2('scontrol show node -a --oneliner')
+
+        raise 'Failed to fetch cluster status' unless status.success?
+
         nodes_hash = Util.scontrol_to_hash(output)
-        
+
         nodes_hash.filter_map do |node|
-          alloctres_hash = node["AllocTRES"].split(',').map { |pair| pair.split('=', 2) }.to_h
-          cfgtres_hash = node["CfgTRES"].split(',').map { |pair| pair.split('=', 2) }.to_h
+          alloctres_hash = node['AllocTRES'].split(',').map { |pair| pair.split('=', 2) }.to_h
+          cfgtres_hash = node['CfgTRES'].split(',').map { |pair| pair.split('=', 2) }.to_h
+          partitions = node['Partitions']&.split(',')&.filter { |p| ::Configuration.included_partitions.include?(p) }
           {
-            "NodeName" => node["NodeName"],
-            "State" => node["State"],
-            "Partitions" => node["Partitions"]&.split(",") || [],
-            "CPUAlloc" => node["CPUAlloc"],
-            "CPUTot" => node["CPUTot"],
-            "CPULoad" => node["CPULoad"],
-            "RealMemory" => node["RealMemory"],
-            "AllocMem" => node["AllocMem"],
-            "FreeMem" => node["FreeMem"],
-            "GPUTot" => (cfgtres_hash["gres/gpu"] || 0).to_i,
-            "GPULoad" => (alloctres_hash["gres/gpu"] || 0).to_i
+            'NodeName' => node['NodeName'],
+            'State' => node['State'],
+            'Partitions' => partitions || [],
+            'CPUAlloc' => node['CPUAlloc'],
+            'CPUTot' => node['CPUTot'],
+            'CPULoad' => node['CPULoad'],
+            'RealMemory' => node['RealMemory'],
+            'AllocMem' => node['AllocMem'],
+            'FreeMem' => node['FreeMem'],
+            'GPUTot' => (cfgtres_hash['gres/gpu'] || 0).to_i,
+            'GPULoad' => (alloctres_hash['gres/gpu'] || 0).to_i
           }
         end
       end
@@ -34,4 +33,4 @@ module Api
       render json: { error: e.message }, status: :internal_server_error
     end
   end
-end 
+end
