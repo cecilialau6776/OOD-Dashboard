e671e4c4299038e449c06e6e15272dc8bfbbca00
Cecilia Lau <cecilialau6776@gmail.com>
2026-09-15 12:51:26 -0400

    update maintenance error checking

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 # The controller for dashboard (root) pages /dashboard
 class DashboardController < ApplicationController
   include MotdConcern
-  
+
   def index
     begin
       set_motd
@@ -15,22 +15,31 @@ class DashboardController < ApplicationController
   def logout; end
 
   def set_maintenance
-    @maintenance = Rails.cache.fetch('next_maintenance', expires_in: 1.hours, race_condition_ttl: 5.seconds) do
-      next_maintenance_json, exit_code = Open3.capture2(%q[scontrol show reservations --json | jq '[ .reservations[] | select((.flags | index("MAINT"))) ] | min_by(.start_time.number)'])
+    maintenance_times = Rails.cache.fetch('next_maintenance', expires_in: 1.hours, race_condition_ttl: 5.seconds) do
+      err_output = { error: 'Error running getting next maintenance window' }
+      begin
+        next_maintenance_json, exit_code = Open3.capture2(%q[scontrol show reservations --json | jq '[ .reservations[] | select((.flags | index("MAINT"))) ] | min_by(.start_time.number)'])
+        return err_output unless exit_code.success?
 
-      return 'Error running getting next maintenance window' unless exit_code.success?
-
-      next_maintenance_json = JSON.parse(next_maintenance_json)
-
-      now = Time.now
+        next_maintenance_json = JSON.parse(next_maintenance_json)
+      rescue JSON::ParserError
+        return err_output
+      end
       start_time = Time.at(next_maintenance_json['start_time']['number'])
       end_time = Time.at(next_maintenance_json['end_time']['number'])
+      { start: start_time, end: end_time }
+    end
 
-      if now < start_time
-        "Next maintenance window on #{start_time.strftime('%b %-d, %Y at %-I:%M %p')}"
-      else
-        "Cluster currently under maintenance. Should end by #{end_time.strftime('%-I:%M %p on %b %-d, %Y')}"
-      end
+    if maintenance_times[:error]
+      Rails.cache.delete('next_maintenance')
+      return maintenance_times[:error]
+    end
+
+    now = Time.now
+    if now < start_time
+      "Next maintenance window on #{maintenance_times[:start].strftime('%b %-d, %Y at %-I:%M %p')}"
+    else
+      "Cluster currently under maintenance. Should end by #{maintenance_times[:end].strftime('%-I:%M %p on %b %-d, %Y')}"
     end
   end
 end
